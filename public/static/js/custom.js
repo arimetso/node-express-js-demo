aecd55a47ef9599b032ad80548b6cfeb09f8af88
arimetso <github@metson.org>
2012-07-20 12:33:49 +0300

    Support for login request.

--- public/static/js/custom.js
+++ public/static/js/custom.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
     $("#signin").click(function() {
         $("#signin-modal").modal();
     });
+    $("#signin-button").click(function() {
+        $("#signin-form").submit();
+    });
 });
 
--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(":memory:");
 
 db.serialize(function() {
-    db.run("create table user (id integer primary key autoincrement, login text, password text)");
-    var stmt = db.prepare("insert into user (login, password) values (?, ?)");
-    stmt.run("juuseri", "passu0");
+    db.run("create table user (id integer primary key autoincrement, login text, password text, fullname text)");
+    var stmt = db.prepare("insert into user (login, password, fullname) values (?, ?, ?)");
+    stmt.run("juuseri", "passu0", "Testi I. Juuseri");
     stmt.finalize();
 });
 
@@ -21,6 +21,9 @@ app.configure(function() {
     app.use(express.static(__dirname + "/public"));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({ secret: "23fc88c72a02e801223813723ed012f6" }));
 
 app.set('view options', {
   layout: false
@@ -29,7 +32,30 @@ app.set('view options', {
 app.get("/", function(req, res) {
     res.render("index.jade", {
         title: "Node.js + Express + Jade",
-        user: null
+        user: req.session.user
+    });
+});
+
+app.post("/login", function(req, res) {
+    var username = req.param("login_name"),
+        password = req.param("login_passwd");
+    if (username == undefined || password == undefined) {
+        console.log("Login failed: no username or password");
+        res.send(400);
+        return;
+    }
+
+    db.get("select id, fullname from user where login = ? and password = ?", [ username, password ], function(err, row) {
+        if (row) {
+            req.session.user = { id: row.id, name: row.fullname };
+            res.redirect('back');
+        } else if (err) {
+            console.log("Login failed due to SQLite error", err);
+            res.send(400);
+        } else {
+            console.log("Login failed: user not found.");
+            res.send(403);
+        }
     });
 });
 
